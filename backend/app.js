32cb67c435d41d2b7fdde7de717c58c84d2b8c15
abarjis <abarjis@users.noreply.github.com>
2021-03-03 00:33:57 -0600

    Added middleware error to app.js

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,22 @@ const app = express();
 
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
+const errorMiddleware = require('./middlewares/errors')
+
 
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// All routes
+/*
+Routes
+*/ 
 const products = require("./routes/product")
+
+/*
+Middleware error
+*/
+app.use(errorMiddleware);
 
 
 app.use('/api/v1', products)
