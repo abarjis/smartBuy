38b01d4b4243534ae9de921a96d19d0a1f616a09
abarjis <abarjis@users.noreply.github.com>
2021-03-02 23:19:09 -0600

    Added deleteProduct Route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, editProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, editProduct, deleteProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/new').post(newProduct);
 
-router.route('/admin/product/:id').put(editProduct);
+router.route('/admin/product/:id')
+                                .put(editProduct)
+                                .delete(deleteProduct)
+
 
 
 module.exports = router;
