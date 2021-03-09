2dcf39eb134386601ceff86446b565165b388607
abarjis <abarjis@users.noreply.github.com>
2021-03-08 18:59:15 -0600

    added search helpers functions

--- backend/helpers/helpers.js
+++ backend/helpers/helpers.js
@@ -3,4 +3,17 @@ class Helpers {
         this.query = query;
         this.queryStr = queryStr;
     }
+
+    search() {
+        const keyword = this.queryStr.keyword ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: 'i'
+            }
+        } : {}
+
+        this.query = this.query.find({ ...keyword });
+        return this;
+    }
+
 }
