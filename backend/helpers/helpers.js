89ba378a4b9ef29cfd6048394dc31297cd8348c7
abarjis <abarjis@users.noreply.github.com>
2021-03-08 20:32:14 -0600

    Added pagination helper function

--- backend/helpers/helpers.js
+++ backend/helpers/helpers.js
@@ -33,6 +33,14 @@ class Helpers {
         return this;
     }
 
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
+    }
+
 }
 
 
