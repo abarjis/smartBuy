fcce7fe575cfd7e2d67264cade81583b11b3c979
abarjis <abarjis@users.noreply.github.com>
2021-03-09 21:48:30 -0600

    added validation for user or admin

--- backend/middlewares/userAuth.js
+++ backend/middlewares/userAuth.js
@@ -20,3 +20,16 @@ exports.isValidUser = catchAsyncErrors(async (req, res, next) => {
 
     next()
 })
+
+/*
+Valid roles: user or admin
+*/
+exports.validRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new ErrorHandler(`Role (${req.user.role}) is not allowed to acccess this resource`, 403))
+        }
+        next()
+    }
+}
