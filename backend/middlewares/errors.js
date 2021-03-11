bb5ccd07586ddaeae71d98e6c8c8a0faab332da2
abarjis <abarjis@users.noreply.github.com>
2021-03-10 19:44:46 -0600

    Handled more errors in middlewares

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -19,15 +19,15 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 
 
 /*
-Mongoose Object ID Error
- */ 
+    Mongoose Object ID Error
+*/ 
     if (err.name === 'CastError') {
         const message = `Resource not found. Invalid: ${err.path}`
         error = new ErrorHandler(message, 400)
         }
 
 /*
-Mongoose Validation Error
+    Mongoose Validation Error
 */
     if (err.name === 'ValidationError') {
         const message = Object.values(err.errors).map(value => value.message);
@@ -38,6 +38,34 @@ Mongoose Validation Error
         success: false,
         message: error.message || 'Internal Server Error'
     })
+    
+/*
+    Mongoose Duplicate Error
+*/ 
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        error = new ErrorHandler(message, 400)
+        }
+
+/*
+     Invalid JWT error
+*/
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'JSON Web Token is invalid. Try Again!!!'
+        error = new ErrorHandler(message, 400)
+        }
+/*
+    Expired JWT error
+*/
+    if (err.name === 'TokenExpiredError') {
+        const message = 'JSON Web Token is expired. Try Again!!!'
+        error = new ErrorHandler(message, 400)
+        }
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        message: error.message || 'Internal Server Error'
+        })  
 }
 
 }
