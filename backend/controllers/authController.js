31bc7d7b0e8008a17d81f1e158aeb1ee29c16c56
abarjis <abarjis@users.noreply.github.com>
2021-03-10 18:51:45 -0600

    Handled forget and reset password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,8 @@ const User = require('../models/user');
 const ErrorHandler = require('../helpers/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const generateToken = require('../helpers/generateToken')
-
+const sendEmail = require('../helpers/sendEmail')
+const crypto = require('crypto');
 
 /*
 Sign up user */
@@ -66,4 +67,81 @@ exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
         success: true,
         message: 'Logged out'
     })
+})
+
+
+/*
+Reset Password
+*/ 
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create reset password url
+    const resetUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
+
+    try {
+
+        await sendEmail({
+            email: user.email,
+            subject: 'SmartBuy Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500))
+    }
+
+})
+
+// Reset Password   =>  /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    // Setup new password
+    user.password = req.body.password;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    generateToken(user, 200, res)
+
 })
