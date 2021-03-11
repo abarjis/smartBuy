2ca504e17b00f0ddb7ff1f53d512d22570075ff6
abarjis <abarjis@users.noreply.github.com>
2021-03-10 20:33:57 -0600

    Added user profile routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword } = require('../controllers/authController');
+const { registerUser,
+    loginUser,
+    logoutUser,
+    forgotPassword,
+    resetPassword,
+    userProfile } = require('../controllers/authController');
+
+
+    const { isValidUser, validRoles } = require('../middlewares/userAuth');
 
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/profile').get(isValidUser, userProfile)
+
 
 router.route('/password/forgot').post(forgotPassword)
 router.route('/password/reset/:token').put(resetPassword)
