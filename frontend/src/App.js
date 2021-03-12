2e59ad932da3199fa1c0e634e1fff0aef47e607d
abarjis <abarjis@users.noreply.github.com>
2021-03-12 15:00:38 -0600

    Created footer.js and footer.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './components/layout/Navbar.css';
+import './components/layout/Footer.css';
 import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+
+
 
 function App() {
   return (
     <div className="App">
          <Navbar />
-      
+
+          <Footer />
       
       </div>
   );
