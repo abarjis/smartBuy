2efe19b7987de2b0c054afb99dde6768e4921067
abarjis <abarjis@users.noreply.github.com>
2021-03-11 20:08:30 -0600

    single Order function to return a single order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,3 +38,21 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
         order
     })
 })
+
+/*
+Returns a single order!
+*/
+exports.singleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if (!order) {
+        return next(new ErrorHandler('Order does not exist ID', 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+
